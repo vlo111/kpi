76d4a1436d8f6f7502cb6955b4ed493d947913b8
vlo111 <vvcsharp@gmail.com>
2022-10-18 12:42:19 +0400

    fax labels color

--- src/components/project/steps/style.tsx
+++ src/components/project/steps/style.tsx
@@ -15,29 +15,39 @@ export const Container = styled.div`
     text-align: center;
   }
 
-  .ant-steps-item-process > .ant-steps-item-container > .ant-steps-item-icon {
-    background: var(--dark-border-ultramarine);
-    border: none;
-  }
+  .ant-steps-item-process > .ant-steps-item-container {
 
-  .ant-steps-item-finish .ant-steps-item-icon {
-    border-color: var(--dark-border-ultramarine);
+    > .ant-steps-item-content > .ant-steps-item-title {
+      color: var(--dark-2);
+    }
+    
+    > .ant-steps-item-icon {
+      background: var(--dark-border-ultramarine);
+      border: none;
+    }
   }
-
+  
   .ant-steps-finish-icon svg {
     fill: var(--dark-border-ultramarine);
   }
 
-  .ant-steps-item-finish > .ant-steps-item-container > .ant-steps-item-content > .ant-steps-item-title {
-    font-size: 14px;
-    letter-spacing: 0.1px;
-    color: var(--dark-2);
+  .ant-steps-item-finish {
+    
+    .ant-steps-item-icon {
+      border-color: var(--dark-border-ultramarine);
+    }
 
-    &:after {
-      background-color: var(--dark-border-ultramarine);
+    > .ant-steps-item-container > .ant-steps-item-content > .ant-steps-item-title {
+      font-size: 14px;
+      letter-spacing: 0.1px;
+      color: var(--dark-2);
+
+      &:after {
+        background-color: var(--dark-border-ultramarine);
+      }
     }
   }
-  
+
   .steps-content {
     width: 100%;
   }
