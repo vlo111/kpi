28df0dad73f4f58b1eb922d83ae699d6ce401083
vlo111 <vvcsharp@gmail.com>
2022-10-24 11:27:48 +0400

    init project detail

--- src/components/Project/Steps/ProjectDetails/index.tsx
+++ src/components/Project/Steps/ProjectDetails/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Collapse } from 'antd'
+import { CaretRightOutlined } from '@ant-design/icons'
 
-const ProjectDetails: React.FC = () => {
+const { Panel } = Collapse
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`
+
+export const ProjectDetails: React.FC = () => {
+  const onChange: (key: string | string[]) => void = (key: string | string[]) => {
+    console.log(key)
+  }
   return (
-    <div>
-      project details
-    </div>
+        <div>
+            <Collapse
+                defaultActiveKey={['1']}
+                onChange={onChange}
+                expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+                className="site-collapse-custom-collapse"
+            >
+                <Panel header="This is panel header 1" key="1">
+                    <p>{text}</p>
+                </Panel>
+            </Collapse>
+        </div>
   )
 }
-
-export default ProjectDetails
