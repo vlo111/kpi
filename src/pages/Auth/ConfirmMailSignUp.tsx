253eb51307b9ad8394763d575156cf3daee1156a
Gor Gasparyan <Gor.gasparyan@analysed.ai>
2022-10-24 11:50:20 +0400

    email confirm ui

--- src/pages/Auth/ConfirmMailSignUp.tsx
+++ src/pages/Auth/ConfirmMailSignUp.tsx
@@ -2,27 +2,31 @@ import React from 'react'
 import AuthLayout from '../../components/Layout/AuthLayout'
 import { Typography } from 'antd'
 import { useParams } from 'react-router-dom'
+import { ReactComponent as Checked } from '../../assets/icons/checked.svg'
 
 const { Title } = Typography
 
 const ConfirmMailSignUp: React.FC = () => {
   const { email } = useParams()
-  console.log(email, 'emmm')
   return (
     <AuthLayout>
       <div>
-      <Title level={1}
-       style={{
-         fontSize: 'var(--large-hedline-font-size)',
-         color: 'var(--dark-border-ultramarine)',
-         textAlign: 'center',
-         marginBottom: '32px'
-       }}
-       >
-       We&apos;ve Sent An Email To
-      </Title>
-      <div style={ { fontSize: '24px', color: '#68A395' } }>{email}</div>
-      <div style={ { fontSize: 'var(--headline-font-size)' } }>Please make sure</div>
+        <Title level={1}
+          style={{
+            fontSize: 'var(--large-hedline-font-size)',
+            color: 'var(--dark-border-ultramarine)',
+            textAlign: 'center',
+            marginBottom: '32px'
+          }}
+        >
+          We&apos;ve Sent An Email To
+        </Title>
+        <div style={{ fontSize: '24px', color: '#68A395' }}>{email}</div>
+        <div style={{ fontSize: 'var(--headline-font-size)' }}>Please make sure</div>
+        <div style={ { display: 'flex', alignItems: 'center', gap: '7px' } }>
+          <Checked />
+          <div>You&apos;ve entered it<span> correctly</span></div>
+        </div>
       </div>
     </AuthLayout>
   )
