52bcf8d3908a21c2613b1c9f6e5fd65dbc07b0aa
Volodya (*_^) <volodya.vardanyan.93v@gmail.com>
2022-10-27 00:22:42 +0400

    fix ResultAreaContainer

--- src/components/Project/Steps/ProjectInput/index.tsx
+++ src/components/Project/Steps/ProjectInput/index.tsx
@@ -15,13 +15,33 @@ const ResultAreaContainer = styled.div`
   box-shadow: -4px -4px 4px rgb(42 85 120 / 5%), 4px 4px 4px rgb(42 85 120 / 5%);
   border-radius: 20px;
   padding: 32px;
-  
+
   display: flex;
   flex-direction: row;
-  gap: 5px;
+  flex-wrap: wrap;
 
   .ant-collapse-content-box {
     padding: 1rem 1rem 2rem 1rem !important;
+  }
+
+  > div:nth-child(1) {
+    width: 8%;
+  }
+
+  > div:nth-child(2) {
+    width: 25%;
+    
+    textarea {
+      height: 44px !important;
+    }
+  }
+
+  > div:nth-child(3) {
+    width: 25%;
+  }
+
+  > div:nth-child(4) {
+    width: 25%;
   }
 `
 
@@ -29,50 +49,51 @@ const ProjectInfo: React.FC = () => {
   const propsData: IPanelPropData = {
     defaultActiveKey: [ResultArea[0].name],
     expandIcon: ({ isActive }: IPanelIsActive) => (
-        <CaretRightOutlined rotate={isActive ? 90 : 0}/>
+            <CaretRightOutlined rotate={isActive ? 90 : 0}/>
     )
   }
 
   return (
-    <div>
-      <AnsCollapseStyle {...propsData}>
-        <Panel key={ResultArea[0].name} header={ResultArea[0].name}>
+        <div>
+            <AnsCollapseStyle {...propsData}>
+                <Panel key={ResultArea[0].name} header={ResultArea[0].name}>
+                    <ResultAreaContainer>
+                        <Form.Item
+                            name={'Code'}
+                            label="Code"
+                            rules={[{ required: true }]}
+                        >
+                            <AsnInput placeholder="OP1.1"/>
+                        </Form.Item>
+                        <Form.Item
+                            name={'result'}
+                            label="Input expected result statement"
+                            rules={[{ required: true }]}
+                        >
+                            <TextArea
+                                placeholder="individuals with improved technical and soft skills following participation in USG-assisted workforce development programs"/>
+                        </Form.Item>
+                        <Form.Item
+                            name={'Measure'}
+                            label="Measure"
+                            rules={[{ required: true }]}
+                        >
+                            <AsnInput placeholder="Number"/>
+                        </Form.Item>
+                        <Form.Item
+                            name={'Target'}
+                            label="Target"
+                            rules={[{ required: true, min: 0, max: 100 }]}
+                        >
+                            <AsnInput placeholder="100"/>
+                        </Form.Item>
+                        <Button style={{ background: 'white', width: '100%' }} htmlType="submit" value="Create">+Add
+                            Result Area</Button>
+                    </ResultAreaContainer>
 
-          <ResultAreaContainer>
-            <Form.Item
-                name={'Code'}
-                label="Code"
-                rules={[{ required: true }]}
-            >
-              <AsnInput placeholder="Organization name"/>
-            </Form.Item>
-            <Form.Item
-                name={'result'}
-                label="Input expected result statement*"
-                rules={[{ required: true }]}
-            >
-              <TextArea placeholder="individuals with improved technical and soft skills following participation in USG-assisted workforce development programs"/>
-            </Form.Item>
-            <Form.Item
-                name={'l.ddid'}
-                label="First Name"
-                rules={[{ required: true }]}
-            >
-              <AsnInput placeholder="Organization name"/>
-            </Form.Item>
-            <Form.Item
-                name={'l.sdid'}
-                label="First Name"
-                rules={[{ required: true }]}
-            >
-              <AsnInput placeholder="Organization name"/>
-            </Form.Item>
-          </ResultAreaContainer>
-
-          <Button style={{ background: 'white', width: '100%' }} htmlType="submit" value="Create">+Add Result Area</Button>
-        </Panel>
-      </AnsCollapseStyle>
-    </div>
+                </Panel>
+            </AnsCollapseStyle>
+        </div>
   )
 }
 
