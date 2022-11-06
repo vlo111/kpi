84daf6dc1b16f745b9ec8ef1b61c3dcff58171dd
Volodya (*_^) <volodya.vardanyan.93v@gmail.com>
2022-11-06 19:49:08 +0400

    discard activity name in overview

--- src/components/Project/Overview/ActiveName/index.tsx
+++ src/components/Project/Overview/ActiveName/index.tsx
@@ -1,4 +1,28 @@
 import React from 'react'
-const R: any = () => <></>
+import styled from 'styled-components'
+import { ActivityName } from '../../../../types/project'
 
-export default R
+const ActivityNames = styled.div`
+  padding: 16px 29px;
+  font-size: var( --base-font-size);
+  background: var(--dark-6);
+  border-radius: 10px;
+  cursor: pointer;
+`
+const activeName = {
+  borderLeft: '6px solid #2A5578',
+  background: 'var(--white)',
+  boxShadow: '-4px 0px 8px rgba(17, 27, 35, 0.05), -4px -4px 8px rgba(17, 27, 35, 0.05)',
+  transform: 'translate(16px, 0px)',
+  borderRadius: '10px 0px 0px 10px'
+}
+
+const ActiveName: React.FC<ActivityName> = ({ name, active, names }) => {
+  return (
+        <ActivityNames className={`${name}`} style={active !== undefined && names[active] === name ? { ...activeName } : {}}>
+            {name}
+        </ActivityNames>
+  )
+}
+
+export default ActiveName
