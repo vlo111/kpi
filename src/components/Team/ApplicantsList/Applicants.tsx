c3a0b2b79054be8696a9ff617419e61615df9d7f
Armen Davtyan <armen.davyan@analyesd.ai>
2022-11-08 00:22:10 +0400

    styling applicants page

--- src/components/Team/ApplicantsList/Applicants.tsx
+++ src/components/Team/ApplicantsList/Applicants.tsx
@@ -1,8 +1,10 @@
-// import { Avatar, List } from 'antd'
 import React from 'react'
 import { Table } from 'antd'
 import styled from 'styled-components'
 import type { ColumnsType } from 'antd/es/table'
+import { ReactComponent as Eyeview } from '../../../assets/icons/eye.svg'
+import { ReactComponent as TrashSvg } from '../../../assets/icons/trash.svg'
+import { ReactComponent as EditSvg } from '../../../assets/icons/edit.svg'
 
 const ApplicantList = styled.div`
     margin-top: 8px;
@@ -35,6 +37,72 @@ const ApplicantList = styled.div`
                 color: #2A5578;
             }
         }
+        .user_role{
+          display: flex;
+          align-items:center;
+          text-align: center;
+
+          svg {
+            margin-right: 8px;
+          }
+        }
+        h3 {
+          font-weight: 700;
+          font-size: 14px;
+          color: #2A5578;
+          margin: 0;
+        }
+        h2 {
+          color: #263238;
+          font-size: 16px;
+          margin: 0;
+        }
+        .user_status_pending,
+        .user_status_resolved {
+          padding: 4px 4px;
+          font-weight: 700;
+          font-size: 14px;
+          background: rgba(104, 163, 149, 0.2);
+          border-radius: 6px;
+          display: flex;
+          justify-content: center;
+          align-items:  center;
+          text-align: center;
+          margin: 0;
+          max-width: 107px;
+        }
+        .user_status_pending {
+          background: rgba(246, 151, 109, 0.2) !important;
+          color: #F6976D;
+        }
+        .users_edit_icons{
+          display: flex;
+          justify-content: center;
+          align-items:center;
+
+          svg {
+            &:last-child{
+              margin-left: 13px;
+            }
+          }
+        }
+    }
+    .ant-table-thead>tr>th{
+      font-weight: 400 !important;
+      font-size: 12px !important;
+      background: var(--background) !important;
+      color: var(--dark-4);
+      border-top: 0.5px solid #EBEBEB;
+
+      &::before{
+        content: none !important;
+      }
+      &:last-child{
+        border-right: 0.5px solid #EBEBEB;
+      }
+      &:first-child{
+        border-left: 0.5px solid #EBEBEB;
+      }
     }
 `
 
@@ -61,15 +129,41 @@ const columns: ColumnsType<DataType> = [
   },
   {
     title: 'Email',
-    dataIndex: 'email'
+    dataIndex: 'email',
+    render: item => {
+      return <h2>{item}</h2>
+    }
   },
   {
     title: 'Access level',
-    dataIndex: 'viewLevel'
+    dataIndex: 'viewLevel',
+    render: item => {
+      return (
+      <div className='user_role'>
+       <Eyeview />
+       <h3>{item}</h3>
+      </div>
+      )
+    }
   },
   {
     title: 'User status',
-    dataIndex: 'status'
+    dataIndex: 'status',
+    render: item => {
+      return (
+        <p className={`${item === 'Pending' ? 'user_status_pending' : 'user_status_resolved'}`}>{item}</p>
+      )
+    }
+  },
+  {
+    render: () => {
+      return (
+        <div className='users_edit_icons'>
+          <EditSvg />
+          <TrashSvg />
+        </div>
+      )
+    }
   }
 ]
 
