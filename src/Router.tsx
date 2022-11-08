1beff841fae4239e32c73971cf3d0bc8a124d622
vlo111 <vvcsharp@gmail.com>
2022-11-08 17:51:45 +0400

    fix main layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate
+} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Error403 from './pages/error-pages/Error403'
 import SignIn from './pages/Auth/SignIn'
@@ -13,24 +18,30 @@ import { PATHS } from './helpers/constants'
 import { PublicRoutes } from './utils/PublicRoutes'
 import { CreateProject } from './pages/CreateProject'
 import { Project } from './components/Project'
+import { PrivateRoutes } from './utils/PrivateRoutes'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route element={<PublicRoutes />}>
         <Route path={PATHS.SIGNIN} element={<SignIn />} />
+        <Route path={PATHS.ERROR_403} element={<Error403 />} />
+        <Route path={PATHS.FORGOTPASSWORD} element={<ForgotPassword />} />
+        <Route path={PATHS.RECOVERPASSWORD} element={<RecoverPassword />} />
+        <Route path={PATHS.ConfirmEmail} element={<ConfirmEmail />} />
+        <Route path={PATHS.SIGNUP} element={<SignUp />} />
+        <Route
+          path={PATHS.ConfirmEmailSignUp}
+          element={<ConfirmMailSignUp />}
+        />
+        <Route path="*" element={<Navigate to={PATHS.ERROR_403} replace />} />
       </Route>
-      <Route path={PATHS.ROOT} element={<Project />} />
-      <Route path={PATHS.CreateProject} element={<CreateProject />} />
-      <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
-      <Route path={PATHS.ERROR_403} element={<Error403 />} />
-      <Route path={PATHS.FORGOTPASSWORD} element={<ForgotPassword />} />
-      <Route path={PATHS.RECOVERPASSWORD} element={<RecoverPassword />} />
-      <Route path={PATHS.ConfirmEmail} element={<ConfirmEmail />} />
-      <Route path={PATHS.SIGNUP} element={<SignUp />} />
-      <Route path={PATHS.ConfirmEmailSignUp} element={<ConfirmMailSignUp />} />
-      <Route path={PATHS.Overview} element={<Overview />} />
-      <Route path="*" element={<Navigate to={PATHS.ERROR_403} replace />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path={PATHS.ROOT} element={<Project />} />
+        <Route path={PATHS.CreateProject} element={<CreateProject />} />
+        <Route path={PATHS.Overview} element={<Overview />} />
+        <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
+      </Route>
     </>
   )
 )
