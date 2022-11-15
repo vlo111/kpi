6a7a2f33c6b49a5f55588d7e09759be74110575b
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2022-11-15 16:55:08 +0400

    header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,26 +16,33 @@ const HeaderLayout = styled(Layout)`
   min-height: 60px;
 `
 
+const HeaderMenu = styled(Layout)`
+  justify-content: end;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 56px;
+  background: var(--white);
+  box-shadow: var(--header-box-shadow);
+`
+
 export const Header: React.FC = () => {
   const navigate = useNavigate()
   return (
     <HeaderLayout>
-      <Row justify="end" align="bottom" gutter={16} style={{ marginRight: '4vw' }} >
-        <Col>
+      <HeaderMenu>
         <Notification />
-        </Col>
-        <Col>
         <Setting />
-        </Col>
-        <Row style={{ gridGap: '16px', cursor: 'pointer', height: '5vh', padding: '6px 0' }} onClick={() => navigate('/user-profile')}>
+        <Row align="middle" style={{ gridGap: '16px', cursor: 'pointer' }} onClick={() => navigate('/user-profile')}>
           <ManagerIcon letter="HD" color="#F3C262" />
           <Row >
-            <Col span={24}>
+            <Col span={80}>
           <Title level={5} style={{ color: 'var(--dark-border-ultramarine)', margin: 0 }}>Anun Azganun</Title>
           <Title level={5} style={{ fontSize: 'var(--font-size-small)', color: 'var(--secondary-light-orage)', marginTop: 0 }}>Ayaho@yaho.yaho</Title></Col>
           </Row>
         </Row>
-      </Row>
+      </HeaderMenu>
     </HeaderLayout>
   )
 }
