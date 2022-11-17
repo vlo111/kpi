fc56c6e44b687e46375546f2fadd745395137210
vlo111 <vvcsharp@gmail.com>
2022-11-17 11:34:07 +0400

    last form

--- src/components/Project/Steps/Last/Items/index.tsx
+++ src/components/Project/Steps/Last/Items/index.tsx
@@ -4,7 +4,6 @@ import { Panel } from '../../../../Forms/AsnCollapse'
 import { Col, Row } from 'antd'
 import AsnInput from '../../../../Forms/Input'
 import { AsnButton } from '../../../../Forms/Button'
-import { IDetail } from '../../../../../types/project'
 import { ReactComponent as DeleteSvg } from '../../../../../assets/icons/delete.svg'
 import { Form } from '../../../../Forms/Form'
 import { rules } from '../../../../../utils/ProjectUtils'
@@ -18,41 +17,6 @@ const FormList = styled(Form.List)`
   }
 `
 
-// export const Items: React.FC<{
-//   header: string
-//   items: IDetail[]
-//   addItemHandle: () => void
-//   deleteItemHandle: (data: string[]) => void
-// }> = ({ header, items, addItemHandle, deleteItemHandle }) => {
-//   return (
-//     <AsnCollapse key={header} id={header}>
-//       <Panel key={header} className="input-rows" header={header}>
-//         {items.map((r: IDetail, i: number) => (
-//           <Row key={`${r.id}${i}`}>
-//             <Form.Item
-//               style={{ width: items.length > 1 ? '99%' : '100%' }}
-//               key={r.id}
-//               name={r.id}
-//               {...rules(2, 256)}
-//             >
-//               <AsnInput placeholder="Organisation name" />
-//             </Form.Item>
-//             {items.length > 1 && (
-//               <div
-//                 className="delete-result"
-//                 onClick={() => deleteItemHandle([header, r.id])}
-//               >
-//                 <DeleteSvg />
-//               </div>
-//             )}
-//           </Row>
-//         ))}
-//         <AsnButton onClick={addItemHandle}>+Add Organizations</AsnButton>
-//       </Panel>
-//     </AsnCollapse>
-//   )
-// }
-
 export const Items: React.FC<{
   name: string
 }> = ({ name }) => {
@@ -62,42 +26,8 @@ export const Items: React.FC<{
             <>
                 <AsnCollapse key={name} id={name}>
                     <Panel key={name} className="input-rows" header={name}>
-                        {/* {items.map((r: IDetail, i: number) => ( */}
-                        {/*    <Row key={`${r.id}${i}`}> */}
-                        {/*        <Form.Item */}
-                        {/*            style={{ width: items.length > 1 ? '99%' : '100%' }} */}
-                        {/*            name={r.id} */}
-                        {/*            {...rules(2, 256)} */}
-                        {/*        > */}
-                        {/*            <AsnInput placeholder="Organisation name" /> */}
-                        {/*        </Form.Item> */}
-                        {/*        {items.length > 1 && ( */}
-                        {/*            <div */}
-                        {/*                className="delete-result" */}
-                        {/*                onClick={() => deleteItemHandle([header, r.id])} */}
-                        {/*            > */}
-                        {/*                <DeleteSvg /> */}
-                        {/*            </div> */}
-                        {/*        )} */}
-                        {/*    </Row> */}
-                        {/* ))} */}
                         {fields.map((field, index) => (
                             <Row key={index}>
-                                {/* <Form.Item */}
-                                {/*    style={{ width: items.length > 1 ? '99%' : '100%' }} */}
-                                {/*    name={r.id} */}
-                                {/*    {...rules(2, 256)} */}
-                                {/* > */}
-                                {/*    <AsnInput placeholder="Organisation name" /> */}
-                                {/* </Form.Item> */}
-                                {/* {items.length > 1 && ( */}
-                                {/*    <div */}
-                                {/*        className="delete-result" */}
-                                {/*        onClick={() => deleteItemHandle([header, r.id])} */}
-                                {/*    > */}
-                                {/*        <DeleteSvg /> */}
-                                {/*    </div> */}
-                                {/* )} */}
                                 <Form.Item
                                     required={false}
                                     key={field.key}
@@ -133,8 +63,7 @@ export const Items: React.FC<{
                                     </AsnButton>
                                 </Form.Item>
                             </Col>
-                        </Row>/
-
+                        </Row>
                     </Panel>
                 </AsnCollapse>
             </>
--- src/components/Project/Steps/Last/index.tsx
+++ src/components/Project/Steps/Last/index.tsx
@@ -1,157 +1,19 @@
 import React from 'react'
-// import styled from 'styled-components'
-import { AsnButton } from '../../../Forms/Button'
-// import { Details, IDetail } from '../../../../types/project'
-// import { useProject } from '../../../../hooks/project/useProject'
-import { ReactComponent as DeleteSvg } from '../../../../assets/icons/delete.svg'
-// import {
-//   OrganizationList,
-//   RegionList,
-//   SectorList
-// } from '../../../../helpers/fakeData'
-// import { ConfirmModal } from '../../../Forms/Modal/ConfirmModal'
-// import { VALIDATE_MESSAGES } from '../../../../helpers/constants'
+
 import { Form } from '../../../Forms/Form'
-// import { v4 as uuidv4 } from 'uuid'
-// import { Items } from './Items'
-import AsnInput from '../../../Forms/Input'
-import { Items } from './Items'
 import styled from 'styled-components'
+import { Items } from './Items'
 
 const Collapses = styled.div`
-  //display: flex;
-  //flex-direction: column;
-  //gap: clamp(5px, 2vw, 20px);
-  //
-  //.input-rows {
-  //  .ant-collapse-content-box {
-  //    display: flex;
-  //    flex-direction: column;
-  //    gap: 1rem;
-  //    padding: 2rem 3.25rem;
-  //
-  //    .delete-result {
-  //      display: flex;
-  //      justify-content: flex-end;
-  //      width: 1%;
-  //      margin-top: 1rem;
-  //
-  //      svg {
-  //        cursor: pointer;
-  //      }
-  //    }
-  //  }
-  //}
-  //
-  //.footer {
-  //  display: flex;
-  //  justify-content: end;
-  //  gap: 5rem;
-  //
-  //  button {
-  //    height: 44px !important;
-  //    box-shadow: 0 4px 4px rgba(42, 85, 120, 0.05);
-  //    border-radius: 6px;
-  //  }
-  //}
 `
 
 export const Last: React.FC = () => {
-  // const [organizations, setOrganizations] = useState<Details>(OrganizationList)
-  // const [regions, setRegions] = useState(RegionList)
-  // const [sectors, setSectors] = useState(SectorList)
-  //
-  // const [openDeleteResultModal, setOpenDeleteResultModal] = useState(false)
-  // const [selectDeleteId, setSelectDeleteId] = useState<string[]>([])
-  //
-  // const { prevCurrent } = useProject()
-
   const [form] = Form.useForm()
-
-  // const initFields: (
-  //   data: IDetail[]
-  // ) => Array<{ name: string[], value: string[] }> = (data: IDetail[]) => {
-  //   const field = form.getFieldsValue()
-  //
-  //   return data.map((o: IDetail) => ({
-  //     name: [o.id],
-  //     value: field[o.id] ? field[o.id] : [o.name]
-  //   }))
-  // }
-
-  console.log(form.getFieldsValue())
-
-  // const fields = [
-  //   ...initFields(organizations),
-  //   ...initFields(regions),
-  //   ...initFields(sectors)
-  // ]
 
   const onFinish: any = (values: any) => {
     console.log(values, 'finish')
     // publish
   }
-
-  // const onFinishFailed: any = (values: any) => {
-  //   console.log(values, 'failed')
-  // }
-
-  // const addOrganisation: () => void = () => {
-  //   const org = {
-  //     id: uuidv4(),
-  //     name: ''
-  //   }
-  //
-  //   const orgs: IDetail[] = organizations.slice(0)
-  //
-  //   orgs.push(org)
-  //
-  //   setOrganizations(orgs)
-  // }
-
-  // const addRegions: () => void = () => {
-  //   const reg = {
-  //     id: uuidv4(),
-  //     name: ''
-  //   }
-  //
-  //   const regs: IDetail[] = regions.slice(0)
-  //
-  //   regs.push(reg)
-  //
-  //   setRegions(regs)
-  // }
-
-  // const addSectors: () => void = () => {
-  //   const sec = {
-  //     id: uuidv4(),
-  //     name: ''
-  //   }
-  //
-  //   const sects: IDetail[] = sectors.slice(0)
-  //
-  //   sects.push(sec)
-  //
-  //   setSectors(sects)
-  // }
-
-  // const deleteResultHandle: (item: string[]) => void = (item) => {
-  //   if (item[0] === 'Organisations') {
-  //     const orgs = organizations.slice(0)
-  //     setOrganizations(orgs.filter((o) => o.id !== item[1]))
-  //   } else if (item[0] === 'Regions') {
-  //     const regs = regions.slice(0)
-  //     setRegions(regs.filter((r) => r.id !== item[1]))
-  //   } else {
-  //     const sects = sectors.slice(0)
-  //     setSectors(sects.filter((s) => s.id !== item[1]))
-  //   }
-  // }
-  //
-  // const deleteItem: (data: string[]) => void = (data) => {
-  //   setOpenDeleteResultModal(!openDeleteResultModal)
-  //   setSelectDeleteId([data[0], data[1]])
-  // }
 
   return (
     <Collapses>
@@ -163,47 +25,3 @@ export const Last: React.FC = () => {
     </Collapses>
   )
 }
-
-/*
-    <>
-      <Form
-        form={form}
-        layout="vertical"
-        fields={fields}
-        validateMessages={VALIDATE_MESSAGES_PROJECT_INPUT}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Collapses>
-          <Items items={organizations} header={'Organisations'} addItemHandle={addOrganisation} deleteItemHandle={deleteItem} />
-          <Items items={regions} header={'Regions'} addItemHandle={addRegions} deleteItemHandle={deleteItem} />
-          <Items items={sectors} header={'Sectors'} addItemHandle={addSectors} deleteItemHandle={deleteItem} />
-          <div className="footer">
-            <AsnButton
-              onClick={() => {
-                prevCurrent()
-              }}
-            >
-              Previous
-            </AsnButton>
-            <AsnButton>Save as Draft</AsnButton>
-            <AsnButton type="primary" htmlType="submit">
-              Publish
-            </AsnButton>
-          </div>
-        </Collapses>
-      </Form>
-      <ConfirmModal
-        styles={{ gap: '6rem' }}
-        yes="Delete"
-        no="Cancel"
-        open={openDeleteResultModal}
-        title="Are you sure you want to delete  the field?"
-        onSubmit={() => {
-          deleteResultHandle(selectDeleteId)
-          setOpenDeleteResultModal(!openDeleteResultModal)
-        }}
-        onCancel={() => setOpenDeleteResultModal(!openDeleteResultModal)}
-      />
-    </>
- */
