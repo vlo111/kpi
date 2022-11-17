f74cfeca504954c38599db8fea5f67f20192b03f
vlo111 <vvcsharp@gmail.com>
2022-11-17 11:57:07 +0400

    fix project forms

--- src/components/Project/Steps/Last/Items/index.tsx
+++ src/components/Project/Steps/Last/Items/index.tsx
@@ -21,53 +21,56 @@ export const Items: React.FC<{
   name: string
 }> = ({ name }) => {
   return (
-      <FormList name={name}>
-        {(fields: any[], { add, remove }: any, { errors }: any) => (
-            <>
-                <AsnCollapse key={name} id={name}>
-                    <Panel key={name} className="input-rows" header={name}>
-                        {fields.map((field, index) => (
-                            <Row key={index}>
-                                <Form.Item
-                                    required={false}
-                                    key={field.key}
-                                >
-                                    <Form.Item
-                                        {...field}
-                                        validateTrigger={['onChange', 'onBlur']}
-                                        {...rules(2, 256)}
-                                        noStyle
-                                    >
-                                        <AsnInput placeholder="passenger name" />
-                                    </Form.Item>
-                                    {fields.length > 1
-                                      ? (
-                                          <div className="delete-result">
-                                              <DeleteSvg
-                                                  className="dynamic-delete-button"
-                                                  onClick={() => remove(field.name)}
-                                              />
-                                          </div>
-                                        )
-                                      : null}
-                                </Form.Item>
-                            </Row>
-                        ))}
+    <FormList name={name}>
+      {(fields: any[], { add, remove }: any, { errors }: any) => (
+        <Row>
+          <Col span={24}>
+            <AsnCollapse key={name} id={name}>
+              <Panel key={name} className="input-rows" header={name}>
+                {fields.map((field, index) => (
+                  <Row key={index}>
+                    <Col span={24}>
+                      <Form.Item required={false} key={field.key}>
                         <Row>
-                            <Col>
-                                <Form.Item>
-                                    <AsnButton
-                                        onClick={() => add()}
-                                    >
-                                        +Add {name}
-                                    </AsnButton>
-                                </Form.Item>
-                            </Col>
+                          <Col span={index ? 23 : 24}>
+                            <Form.Item
+                              {...field}
+                              validateTrigger={['onChange', 'onBlur']}
+                              {...rules(2, 256)}
+                              noStyle
+                            >
+                              <AsnInput placeholder="passenger name" />
+                            </Form.Item>
+                          </Col>
+                          <Col span={1}>
+                            {fields.length > 1
+                              ? (
+                              <div className="delete-result">
+                                <DeleteSvg
+                                  className="dynamic-delete-button"
+                                  onClick={() => remove(field.name)}
+                                />
+                              </div>
+                                )
+                              : null}
+                          </Col>
                         </Row>
-                    </Panel>
-                </AsnCollapse>
-            </>
-        )}
-      </FormList>
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                ))}
+                <Row>
+                  <Col>
+                    <Form.Item>
+                      <AsnButton onClick={() => add()}>+Add {name}</AsnButton>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Panel>
+            </AsnCollapse>
+          </Col>
+        </Row>
+      )}
+    </FormList>
   )
 }
