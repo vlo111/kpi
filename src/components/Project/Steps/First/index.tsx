7b1d0a32f5837124bdd5206864e287c63e0c1176
vlo111 <vvcsharp@gmail.com>
2022-11-22 19:36:23 +0400

    fix areaBox form

--- src/components/Project/Steps/First/index.tsx
+++ src/components/Project/Steps/First/index.tsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import InputResult from './InputResult'
 import { VALIDATE_MESSAGES_PROJECT_INPUT } from '../../../../helpers/constants'
-import { Row } from 'antd'
 import { AsnButton } from '../../../Forms/Button'
 import { Form } from '../../../Forms/Form'
 import { ProjectInputForm } from '../../../Forms/ProjectInputForm'
-import { initFields } from '../../../../utils/ProjectUtils'
-import { useProjectInput } from '../../../../hooks/project/useProjectInput'
 import { useProject } from '../../../../hooks/project/useProject'
+
+const setTitleColor: (element: HTMLElement, color: string) => void = (element, color) => {
+  if (element) {
+    const titleElement = element.firstChild as HTMLElement
+
+    const pathElement = element.lastChild?.firstChild as HTMLElement
+
+    if (titleElement) {
+      titleElement.style.color = color
+    }
+
+    if (pathElement) {
+      pathElement.style.fill = color
+    }
+  }
+}
+
+const setError: () => void = () => {
+  // @ts-expect-error
+  const errorsIndex = [...new Set(values.errorFields.map((r) => r.name[1]))]
+
+  const resultAreaElement: (id: string) => void = (id) => {
+    const resultAreaElement = document.getElementById(`ans-title-${id}`) as HTMLElement
+
+    setTitleColor(resultAreaElement, 'var(--error)')
+  }
+
+  const resultAreaElements = document.getElementsByClassName('result_area_title') as HTMLCollectionOf<HTMLElement>
+
+  if (resultAreaElements) {
+    Array.from(resultAreaElements).forEach(element => {
+      setTitleColor(element, 'var(--dark-2)')
+    })
+  }
+
+  errorsIndex.map((i) => resultAreaElement(i))
+}
 
 export const First: React.FC = () => {
   const { nextCurrent, prevCurrent } = useProject()
-  const { resultArea, addNewResultArea } = useProjectInput()
   const [form] = Form.useForm()
-
-  const fields = initFields(form.getFieldsValue(), resultArea)
 
   const onFinish: any = (values: any) => {
     console.log(values, 'finish')
@@ -23,36 +54,41 @@ export const First: React.FC = () => {
 
   const onFinishFailed: any = (values: any) => {
     console.log(values, 'failed')
+
+    setError()
   }
 
   return (
-        <ProjectInputForm
-            form={form}
-            layout="vertical"
-            fields={fields}
-            validateMessages={VALIDATE_MESSAGES_PROJECT_INPUT}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
+    <ProjectInputForm
+      form={form}
+      initialValues={{
+        result_area_form: [
+          {
+            resultAreaInput: '',
+            expectedList: [{}],
+            activities: [{ activityInput: '', milestones: [{}] }]
+          }
+        ]
+      }}
+      layout="vertical"
+      validateMessages={VALIDATE_MESSAGES_PROJECT_INPUT}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <InputResult />
+      <div className="footer">
+        <AsnButton
+          onClick={() => {
+            prevCurrent()
+          }}
         >
-            <InputResult form={form} resultArea={resultArea}/>
-            <Row>
-                <AsnButton
-                    style={{ background: 'white', width: '100%', height: '3rem' }}
-                    value="Create"
-                    onClick={() => addNewResultArea()}
-                >
-                    +Add Result Area
-                </AsnButton>
-            </Row>
-            <div className="footer">
-                <AsnButton onClick={() => {
-                  prevCurrent()
-                }}>Cancel</AsnButton>
-                <AsnButton>Save as Draft</AsnButton>
-                <AsnButton type="primary" htmlType="submit">
-                    Next
-                </AsnButton>
-            </div>
-        </ProjectInputForm>
+          Cancel
+        </AsnButton>
+        <AsnButton>Save as Draft</AsnButton>
+        <AsnButton type="primary" htmlType="submit">
+          Next
+        </AsnButton>
+      </div>
+    </ProjectInputForm>
   )
 }
