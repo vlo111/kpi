f7e19e5316891376953304a34422d622439d96d7
vlo111 <vvcsharp@gmail.com>
2022-12-05 19:38:13 +0400

    fix user profile component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
-import { AsnForm } from '../Form';
+import { AsnForm } from '../Forms/Form';
 
 export const CreateTemplateContainer = styled(AsnForm)`
-     background:var(--white);
-    border-top: 3px solid var(--dark-border-ultramarine);
-    box-shadow: var(--base-box-shadow);
-    border-radius: 20px;
-    margin: 6.3vw 9vw 0px 9vw;
-  .ant-btn-icon-only{
+  background: var(--white);
+  border-top: 3px solid var(--dark-border-ultramarine);
+  box-shadow: var(--base-box-shadow);
+  border-radius: 20px;
+  margin: 6.3vw 9vw 0px 9vw;
+  
+  .ant-btn-icon-only {
     border-radius: 50%;
     position: absolute;
     left: 86px;
--- src/components/Users/UserProfile/index.tsx
+++ src/components/Users/UserProfile/index.tsx
@@ -7,7 +7,7 @@ import { PATHS } from '../../../helpers/constants';
 import { TVoid } from '../../../types/global';
 import { IUser } from '../../../types/auth';
 import AsnAvatar from '../../Forms/Avatar';
-import { CreateTemplateContainer } from '../../Forms/UserProfile';
+import { CreateTemplateContainer } from '../../Profile';
 import { AsnButton } from '../../Forms/Button';
 import EditProfile from '../EditUserProfile';
 import { ReactComponent as UploadUser } from '../../../assets/icons/upload.svg';
@@ -18,14 +18,14 @@ const UserProfile: React.FC = () => {
   const { data: user }: { data: IUser } = useCurrentUser();
   const navigate = useNavigate();
   const [isOpenCreateActivityModal, setIsOpenCreateActivityModal] =
-  useState<boolean>(false);
+    useState<boolean>(false);
 
   const onEditedPublishProject: TVoid = () => {
     setIsOpenCreateActivityModal(true);
   };
   return (
     <CreateTemplateContainer>
-         <Col
+      <Col
         style={{
           top: '4vh',
           marginLeft: 'auto',
@@ -35,37 +35,41 @@ const UserProfile: React.FC = () => {
         }}
       >
         {' '}
-        <Button type='link' onClick={onEditedPublishProject}>
+        <Button type="link" onClick={onEditedPublishProject}>
           <Edit style={{ height: '24px', width: '24px' }} />
         </Button>
         <EditProfile
-              isOpenCreateActivityModal={isOpenCreateActivityModal}
-              setIsOpenCreateActivityModal={setIsOpenCreateActivityModal}
-              user={user}
-            />
+          isOpenCreateActivityModal={isOpenCreateActivityModal}
+          setIsOpenCreateActivityModal={setIsOpenCreateActivityModal}
+          user={user}
+        />
       </Col>
-      <Row gutter={[0, 32]} style={{ padding: '64px 60px' }} >
+      <Row gutter={[0, 32]} style={{ padding: '64px 60px' }}>
         <Col md={7} xs={12} span={8}>
           <Upload>
             <AsnAvatar
-              letter={`${user?.firstName?.charAt(0)}${user?.lastName?.charAt(0)}`}
+              letter={`${user?.firstName?.charAt(0)}${user?.lastName?.charAt(
+                0
+              )}`}
               size={128}
             />
             <Button icon={<UploadUser />}></Button>
           </Upload>
         </Col>
         <Col md={12} xs={24}>
-        <Title
-              level={5}
-              style={{
-                fontSize: 'clamp(1rem, 2.2vw, 3rem)',
-                color: 'var(--dark-border-ultramarine)',
-                fontWeight: 'var(--font-normal)'
-              }}
-            >
-             {user?.firstName} {user?.lastName}
-            </Title>
-          <Row style={{ color: 'var(--dark-2)', fontSize: '20px', gap: '0 30px' }}>
+          <Title
+            level={5}
+            style={{
+              fontSize: 'clamp(1rem, 2.2vw, 3rem)',
+              color: 'var(--dark-border-ultramarine)',
+              fontWeight: 'var(--font-normal)'
+            }}
+          >
+            {user?.firstName} {user?.lastName}
+          </Title>
+          <Row
+            style={{ color: 'var(--dark-2)', fontSize: '20px', gap: '0 30px' }}
+          >
             <Col span={10}>E-mail:</Col>
             <Col span={10}>{user?.email}</Col>
             <Col span={10}>Phone:</Col>
@@ -83,12 +87,12 @@ const UserProfile: React.FC = () => {
           <AsnButton
             type="primary"
             onClick={() => navigate(`/${PATHS.CHANGEPASSWORD}`)}
-             style={{
-               width: 'clamp(13.4rem, 10vw, 21rem)',
-               marginBottom: '1vh',
-               fontSize: 'var(--headline-font-size)',
-               padding: '0'
-             }}
+            style={{
+              width: 'clamp(13.4rem, 10vw, 21rem)',
+              marginBottom: '1vh',
+              fontSize: 'var(--headline-font-size)',
+              padding: '0'
+            }}
           >
             Change Password
           </AsnButton>
