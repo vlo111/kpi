c84a684d4058d5e914c6f9358ce855a8eab93b9a
Analysed Soft <David.Karapetyan1@contracted.pmi.com>
2022-12-09 16:42:36 +0400

    Change endpoint name , structure

--- src/pages/Project/List.tsx
+++ src/pages/Project/List.tsx
@@ -1,65 +1,70 @@
-import React, { FC, ReactNode } from 'react';
-import dayjs from 'dayjs';
-import { Card, Col, Row } from 'antd';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import React, { FC, ReactNode, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, Col, Row, Space, Spin, Typography } from "antd";
+import dayjs from "dayjs";
+import { PATHS } from "../../helpers/constants";
+import { ICreateProject } from "../../types/project";
+import useGetProjects from "../../api/Project/useGetProjects";
 
-import { ICreateProject } from '../../types/project';
-import { useGetProjects } from '../../api/Project/useGetProjects';
-
-const ProjectListRow = styled(Row)`
-  padding: 2rem;
-
-  .ant-col {
-    padding: 1rem;
-  }
-`;
+const { Title } = Typography;
 
 export const ProjectList: FC = () => {
   const navigate = useNavigate();
+  const [result, setResult] = useState({ data: [], totalRecords: 0 });
 
-  const { data: { result = [], has_more: hasMore, count } = {} } = useGetProjects();
-
-  const extraElement: (id: string) => ReactNode = (id) => <a
-    onClick={(e) => {
-      e.preventDefault();
-      navigate(`../${id}`, { replace: true });
-    }}
-    style={{ marginRight: '10px' }}
-  >
-    Edit
-  </a>;
-
+  const { isLoading } = useGetProjects(
+    {},
+    {
+      onSuccess: (data: any) =>
+        setResult((prev) => ({
+          data: data.result,
+          totalRecords: data.count,
+        })),
+      enabled: true,
+    }
+  );
+  const handleEdit = (id: string): void => {
+    navigate(`/${PATHS.PROJECT}/${id}`);
+  };
   return (
-    <>
-      <Row gutter={16}>
-        <Col>Count: {count}</Col>
-        <Col>
-          Has more: {hasMore === null || (hasMore === false) ? 'no' : 'yes'}
-        </Col>
-      </Row>
-      <ProjectListRow gutter={16}>
-        {result?.map((r: ICreateProject) => (
-          <Col span={6} key={r.id}>
-            <Card
-              headStyle={{ background: 'var(--secondary-light-amber)' }}
-              title={r.title}
-              bordered={false}
-              extra={extraElement(r.id)}
-            >
-              <p>
-                <b>Description</b> {r?.description}
-              </p>
-              <p>
-                <b>Start Date</b> {dayjs(r?.startDate).format('DD/MM/YYYY')}
-              </p>
-              <p>
-                <b>End Date</b> {dayjs(r?.endDate).format('DD/MM/YYYY')}
-              </p>
-            </Card>
+    <Spin spinning={isLoading}>
+      <Space direction="vertical">
+        <Row gutter={[16, 16]}>
+          <Col span={4} offset={1}>
+            <Title level={5}>Count: {result?.totalRecords}</Title>
           </Col>
-        ))}
-      </ProjectListRow>
-    </>
+        </Row>
+        <Row gutter={16}>
+          {result?.data?.map((item: ICreateProject) => (
+            <Col span={6} key={item?.id}>
+              <Card
+                headStyle={{ background: "var(--secondary-light-amber)" }}
+                title={
+                  <Space
+                    direction="horizontal"
+                    style={{ width: "100%", justifyContent: "space-between" }}
+                  >
+                    {item?.title}{" "}
+                    <a onClick={() => handleEdit(item?.id)}> Edit</a>
+                  </Space>
+                }
+                bordered={false}
+              >
+                <p>
+                  <b>Description</b> {item?.description}
+                </p>
+                <p>
+                  <b>Start Date</b>
+                  {dayjs(item?.startDate).format("DD/MM/YYYY")}
+                </p>
+                <p>
+                  <b>End Date</b> {dayjs(item?.endDate).format("DD/MM/YYYY")}
+                </p>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Space>
+    </Spin>
   );
 };
