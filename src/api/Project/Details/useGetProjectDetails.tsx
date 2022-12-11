db99d5dfa9b1964b9a055e113a733738528a9c0d
Volodya (*_^) <volodya.vardanyan.93v@gmail.com>
2022-12-11 05:30:41 +0400

    fix project details types

--- src/api/Project/Details/useGetProjectDetails.tsx
+++ src/api/Project/Details/useGetProjectDetails.tsx
@@ -1,19 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import { AxiosResponse } from 'axios';
-import {
-  IResultArea,
-  QueryGetResultArea
-} from '../../../types/api/project/get-project';
-
 import client from '../../client';
+import { UseGetProjectDetails } from '../../../types/api/project/get-project';
 
 const URL_GET_PROJECTS = 'api/project';
 
-export const useGetProjectDetails: (id: string | undefined) => [] | undefined = (id: string | undefined) => {
+export const useGetProjectDetails: UseGetProjectDetails = (id: string | undefined) => {
   try {
     if (id !== undefined) {
-      const { data }: QueryGetResultArea =
-        useQuery<AxiosResponse<IResultArea>, Error>(
+      const { data } =
+        useQuery(
           ['project', id],
           async () => await client.get(`${URL_GET_PROJECTS}/${id}/project-details`)
         );
