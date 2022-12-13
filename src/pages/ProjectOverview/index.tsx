342ab82e98d847322b144c8dae03562cb236b093
Gor Gasparyan <Gor.gasparyan@analysed.ai>
2022-12-13 17:22:24 +0400

    small fix

--- src/pages/ProjectOverview/index.tsx
+++ src/pages/ProjectOverview/index.tsx
@@ -74,7 +74,7 @@ const ProjectOverview: React.FC = () => {
                 children: (
                   <Space direction='vertical' align='center' style={{ width: '100%', padding: '5vh 0 30px 0' }} >
                     <EditPublishSvg />
-                    <AsnButton className='primary' >Edit and Publish the project</AsnButton>
+                    <AsnButton className='primary' onClick={() => navigate(`/project/${(id != null) ? id : ''}/steps/1`)}>Edit and Publish the project</AsnButton>
                   </Space>
                 )
               };
