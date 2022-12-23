299cd7a7cefafd03a41f2e5b4ec657b6b03b91da
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2022-12-23 15:08:05 +0400

    card hover

--- src/pages/ProjectOverview/SubActivites/ActiveTemplate/index.tsx
+++ src/pages/ProjectOverview/SubActivites/ActiveTemplate/index.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 
 import { ConfirmModal } from '../../../../components/Forms/Modal/Confirm';
 import { ISubActivityAndTemplates } from '../../../../types/project';
-import useDuplicateTemplate from '../../../../api/Activity/Template/useDuplicateTemplate';
 import useDeleteActivityTemplate from '../../../../api/Activity/Template/useDeleteActivityTemplate';
 import { ReactComponent as TrashSvg } from '../../../../assets/icons/trash.svg';
 import { ReactComponent as EditSvg } from '../../../../assets/icons/edit.svg';
@@ -25,13 +24,17 @@ const Container = styled.div`
 .ant-card-extra{
     position: absolute;
     top: 0;
-    background-color: rgba(0,0,0,0.2) !important;
+    background-color: #263238 !important;
     height: 196px;
     width: 100%;
     left: 0;
     border-radius: 20px;
-    display: flex;
-    align-items: center;
+    display: flex !important;
+    align-items: center !important;
+    justify-content: center !important;
+    color: white;
+    z-index: 1 !important;
+    opacity: 0.9;
   }
 .ant-card-body {
   padding: 0 45px;
@@ -39,13 +42,12 @@ const Container = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center !important;
-  height: 100%;
+  height: 100% ;
   font-size: var(--base-font-size);
   text-align: center;
   word-wrap: break-word;
-  white-space: initial;
-  -webkit-box-orient: vertical !important;
-  -webkit-line-clamp: 5 !important;
+  position: absolute;
+  top: 0;
 
 }
 .ant-card-head {
@@ -57,16 +59,13 @@ const Container = styled.div`
 }
 .cardClick {
   position: absolute;
-  z-index: 1;
+  z-index: 2 !important;
   right: 20px;
   color: var(--dark-1);
 }
 .ant-card-head-title{
   word-wrap: break-word;
   white-space: initial;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 5;
-   display: -webkit-box;
   }
   .ant-popover-inner-content{
     height: 180px;
@@ -116,16 +115,6 @@ export const ActiveTempalate: React.FC<ISubActivityAndTemplates> = ({ templates,
       }
     }
   );
-  const { mutate: duplicateTemplate } = useDuplicateTemplate(
-    {
-      onSuccess: () => {
-        refetch();
-      },
-      onError: () => {
-        void message.error('Something went wrong', 2);
-      }
-    }
-  );
 
   const title = (id: string): any => {
     return (
@@ -145,7 +134,7 @@ export const ActiveTempalate: React.FC<ISubActivityAndTemplates> = ({ templates,
             <TrashSvg />
             Delete
           </Popup>
-          <Popup type="link" onClick={() => duplicateTemplate({ id })}>
+          <Popup type="link">
             <Dublicat />
             Duplicate
           </Popup>
