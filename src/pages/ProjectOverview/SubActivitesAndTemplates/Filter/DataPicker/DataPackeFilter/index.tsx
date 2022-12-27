023b5efb8d735a3189c9653fbd09ccababc8b955
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2022-12-27 16:13:18 +0400

    dataPicker cards

--- src/pages/ProjectOverview/SubActivitesAndTemplates/Filter/DataPicker/DataPackeFilter/index.tsx
+++ src/pages/ProjectOverview/SubActivitesAndTemplates/Filter/DataPicker/DataPackeFilter/index.tsx
@@ -1,23 +1,53 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react';
-import { ConfigProvider, DatePicker } from 'antd';
+import styled from 'styled-components';
+import { Col, Space } from 'antd';
 
-const { RangePicker } = DatePicker;
+import { AsnForm } from '../../../../../../components/Forms/Form';
+import { AsnDatePicker } from '../../../../../../components/Forms/DatePicker';
 
-const Page = () => {
-  return (
-    <div >
-      <RangePicker style={{ width: 200 }}/>
-    </div>
-  );
-};
+export const PickerSpace = styled(Space)`
+  width: 100%;
+
+  > div {
+    font-size: var(--headline-font-size);
+  }
+
+  .ant-picker,
+  .ant-space-item {
+    width: 100%;
+  }
+
+  .ant-picker-input > input {
+    height: 30px;
+    font-size: var(--base-font-size);
+  }
+`;
 
 export const DateFilter: React.FC = () => {
   return (
-    <div>
-      <ConfigProvider >
-        <Page />
-      </ConfigProvider>
-    </div>
+            <PickerSpace size={18}>
+              <Col span={22}>
+                <AsnForm.Item
+                  name="startDate"
+                  label="Start Date"
+                >
+                  <AsnDatePicker
+                    format="DD/MM/YYYY"
+                    placeholder="01/01/23"
+                  />
+                </AsnForm.Item>
+              </Col>
+              <Col span={22}>
+                <AsnForm.Item
+                  name="endDate"
+                  label="End Date"
+                >
+                  <AsnDatePicker
+                    placeholder="01/01/23"
+                    format="DD/MM/YYYY"
+                  />
+                </AsnForm.Item>
+              </Col>
+            </PickerSpace>
   );
 };
