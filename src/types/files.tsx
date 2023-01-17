f7e510144908c6c05890556678f94795a215396b
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2023-01-17 21:03:34 +0400

    types upload function

--- src/types/files.tsx
+++ src/types/files.tsx
@@ -5,18 +5,43 @@ export interface IFiles {
   path: string
   type: string
 }
+export interface IFolders {
+  id: string
+  title: string
+  sectionDataId: string
+}
 
 export interface ISearchImport {
   files: IFiles[]
+  courseFiles: {
+    folders: IFolders[]
+    files: {
+      GENERAL_DOCUMENT: IFiles[]
+      REQUIRED_DOCUMENT: IFiles[]
+    }
+  }
+  courseId: string | null
+  refetch: any
 }
 export interface IDataResult {
-  fileList: IFiles[]
+  fileList: IFiles[] | ICourseFiles | any
   setOpen: React.Dispatch<React.SetStateAction<string>>
   open: string
   onRemoveFile: (id?: string) => void
+  courseId: string | null
+  refetch: any
 }
 
 export interface IFilesProps {
   allFilesCount: number
-  setFiles: React.Dispatch<React.SetStateAction<IFiles[]>>
+  setCourseId: React.Dispatch<React.SetStateAction<string | null>>
+  courseFiles: ICourseFiles
+}
+
+export interface ICourseFiles {
+  folders: IFolders[]
+  files: {
+    GENERAL_DOCUMENT: IFiles[]
+    REQUIRED_DOCUMENT: IFiles[]
+  }
 }
