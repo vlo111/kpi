1b0b035328a9605118c64aaa79fb0104453368e9
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2023-01-26 20:07:23 +0400

    all filter or backend

--- src/api/Applicants/useGetApplicantsFilter.tsx
+++ src/api/Applicants/useGetApplicantsFilter.tsx
@@ -1,24 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
-import client from '../client';
 
+import { useMutation } from '@tanstack/react-query';
+import client from '../client';
 const url = '/api/applicant';
 
-const useGetApplicantsFilter: any = (id: string, params: object = {}, options = { enabled: false }) => {
-  const result = useQuery(
-    [url, id, params],
-    async () => await client.post(`${url}/${id}`, params),
-    {
-      ...options,
-      select: (data) => data.data
-    }
-  );
-  const { data, isSuccess, isLoading } = result;
-  return {
-    ...result,
-    data: isSuccess ? data?.result : [],
-    isLoading
-    // refetch
-  };
+const useGetApplicantsFilter = (options = {}): any => {
+  const mutation = useMutation((data): any => {
+    return client.post(url, data);
+  }, options);
+  return mutation;
 };
 
 export default useGetApplicantsFilter;
--- src/pages/Applicatns/aplicantsDataTable/index.tsx
+++ src/pages/Applicatns/aplicantsDataTable/index.tsx
@@ -1,7 +1,16 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable no-lone-blocks */
 import React, { useState } from 'react';
-import { Button, Table, Popover, Collapse, Space, Slider, Radio } from 'antd';
+import {
+  Button,
+  Table,
+  Popover,
+  Collapse,
+  Space,
+  Slider,
+  Radio,
+  Form
+} from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { FilterOutlined } from '@ant-design/icons';
 import { Panel } from '../../../components/Forms/Collapse';
@@ -11,8 +20,16 @@ import { AsnCheckboxGroup } from '../../../components/Forms/Checkbox';
 import { ContentAssingersFilter } from '../applicantsStyle';
 import moment from 'moment';
 import { ApplicatnList, DataType } from '../applicantsTypes';
+import useGetApplicantsFilter from '../../../api/Applicants/useGetApplicantsFilter';
 
-const ApplicantsDataList: React.FC<ApplicatnList> = ({ allApplicants, searchAplicant, search }) => {
+const ApplicantsDataList: React.FC<ApplicatnList> = ({
+  allApplicants,
+  searchAplicant,
+  search
+}) => {
+  const [form] = Form.useForm();
+  const { mutate: applicantsFilter } = useGetApplicantsFilter({});
+
   // Age function
   const marks: SliderMarks = {
     0: '0',
@@ -22,141 +39,208 @@ const ApplicantsDataList: React.FC<ApplicatnList> = ({ allApplicants, searchApli
   };
   //
   // gender function
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState();
 
   const onChange = (e: any): any => {
-    console.log('radio checked', e.target.value);
+    console.log(e.target.value);
     setValue(e.target.value);
   };
   ///
   // student function
-  const [valueStudent, setValueStudent] = useState(1);
+  const [valueStudent, setValueStudent] = useState();
 
   const onChangeStudent = (e: any): any => {
-    console.log('radio checked', e.target.value);
+    console.log(e.target.value);
     setValueStudent(e.target.value);
   };
   ///
   // Status fanction
   const onChangeStatus = (checkedValues: any): any => {
-    console.log('checked = ', checkedValues);
+    console.log(checkedValues);
   };
   const optionsStatus = [
-    { label: 'Applicant', value: 'Applicant' },
-    { label: 'Selection', value: 'Selection' },
-    { label: 'Pre-Assessment', value: 'Pre_Assessment' },
-    { label: 'Participant', value: 'Participant' },
-    { label: 'Post-Assessment ', value: 'Post_Assessment ' },
-    { label: 'Trained', value: 'Trained' }
+    { label: 'Applicant', value: 'APPLICANT' },
+    { label: 'Selection', value: 'SELECTION' },
+    { label: 'Pre-Assessment', value: 'PRE_ASSESSMENT' },
+    { label: 'Participant', value: 'PARTICIPANT' },
+    { label: 'Post-Assessment ', value: 'POST_ASSESSMENT' },
+    { label: 'Trained', value: 'TRAINED' }
   ];
   //
 
   // Paid function
-  const [valuePaid, setValuePaid] = useState(1);
+  const [valuePaid, setValuePaid] = useState();
 
   const onChangePaid = (e: any): any => {
-    console.log('radio checked', e.target.value);
+    console.log(e.target.value);
     setValuePaid(e.target.value);
   };
-    ///
+  ///
 
   // Vulnerability function
-  const [valueVulnerability, setValueVulnerability] = useState(1);
+  const [valueVulnerability, setValueVulnerability] = useState();
 
   const onChangeVulnerability = (e: any): any => {
-    console.log('radio checked', e.target.value);
+    console.log(e.target.value);
     setValueVulnerability(e.target.value);
   };
-    ///
-    // Region fanction
+  ///
+  // Region fanction
   const onChangeRegion = (checkedValues: any): any => {
-    console.log('checked = ', checkedValues);
+    console.log(checkedValues);
   };
   const optionsRegion = [
-    { label: 'Yerevan', value: 'Yerevan' },
-    { label: 'Aragatsotn', value: 'Aragatsotn' },
-    { label: 'Ararat', value: 'Ararat' },
-    { label: 'Armavir', value: 'Armavir' },
-    { label: 'Gegharkunik', value: 'Gegharkunik' },
-    { label: 'Kotayk', value: 'Kotayk' },
-    { label: 'Shirak', value: 'Shirak' },
-    { label: 'Syunik', value: 'Syunik' },
-    { label: 'Tavush', value: 'Tavush' },
-    { label: 'Vayots Dzor', value: 'Vayots_Dzor' }
-
+    { label: 'Yerevan', value: 'Yerevan/Երևան,' },
+    { label: 'Aragatsotn', value: 'Aragatsotn/Արագածոտն,' },
+    { label: 'Ararat', value: 'Ararat/Արարատ,' },
+    { label: 'Armavir', value: 'Armavir/Արմավիր,' },
+    { label: 'Gegharkunik', value: 'Gegharkunik/Գեղարքունիք,' },
+    { label: 'Kotayk', value: 'Kotayk/Կոտայք' },
+    { label: 'Lori', value: 'Lori/Լոռի,' },
+    { label: 'Shirak', value: 'Shirak/Շիրակ,' },
+    { label: 'Syunik', value: 'Syunik/Սյունիք,' },
+    { label: 'Tavush', value: 'Tavush/Տավուշ,' },
+    { label: 'Vayots Dzor', value: 'Vayots Dzor/Վայոց Ձոր,' }
   ];
   //
+  const onFinish = (values: any): any => {
+    console.log(values, 'values');
+    const requestBody = {
+      statuses:
+        values?.status?.map((s: any) => {
+          return (
+            s
+          );
+        }),
+      age:
+      {
+        from: values?.age[0],
+        to: values?.age[1]
+      },
+      regions:
+        values?.region?.map((i: any) => {
+          return (
+            i
+
+          );
+        }),
+      student: values?.student,
+      gender: values?.gender,
+      disability: values?.vulnerability,
+      income: values?.paid_job,
+      limit: 50,
+      offset: 0
+
+    };
+    applicantsFilter(requestBody);
+  };
   const content = (
     <ContentAssingersFilter>
-          <Collapse collapsible="icon" bordered={false} accordion expandIcon={({ isActive }) => (isActive ?? false) ? '-' : '+'}>
-      <Panel header="Age" key="1" className='fffffffffff'>
-          <Slider range marks={marks} defaultValue={[16, 27]}/>
-          {/* tooltip={{ open: true }}  */}
-      </Panel>
-      <Panel header="Gender" key="2">
-      <Radio.Group onChange={onChange} value={value}>
-      <Space direction="vertical">
-        <Radio value={1}>Famale</Radio>
-        <Radio value={2}>Male</Radio>
-      </Space>
-    </Radio.Group>
-      </Panel>
-      <Panel header="Student" key="3">
-      <Radio.Group onChange={onChangeStudent} value={valueStudent}>
-      <Space direction="vertical">
-        <Radio value={1}>Nonstudent</Radio>
-        <Radio value={2}>Student</Radio>
-      </Space>
-    </Radio.Group>
-      </Panel>
-      <Panel header="Status" key="4">
-      <AsnCheckboxGroup
-      style={{ width: '200px' }}
-      options={optionsStatus}
-      onChange={onChangeStatus}
-    />
-      </Panel>
-      <Panel header="Paid job" key="5">
-      <Radio.Group onChange={onChangePaid} value={valuePaid}>
-      <Space direction="vertical">
-        <Radio value={1}>Paid job</Radio>
-        <Radio value={2}>Unemployed</Radio>
-      </Space>
-    </Radio.Group>
-      </Panel>
-      <Panel header="Vulnerability" key="6">
-      <Radio.Group onChange={onChangeVulnerability} value={valueVulnerability}>
-      <Space direction="vertical">
-        <Radio value={1}>NA</Radio>
-        <Radio value={2}>Vulnerable group member</Radio>
-      </Space>
-    </Radio.Group>
-      </Panel>
-      <Panel header="Region" key="7">
-      <AsnCheckboxGroup
-      style={{ width: '160px' }}
-      options={optionsRegion}
-      onChange={onChangeRegion}
-    />
-      </Panel>
-    </Collapse>
-    <Space direction='horizontal' size={[10, 0]} style={{ justifyContent: 'space-around', width: '100%', padding: '30px 0 0px 0px' }}>
-          <AsnButton className="default">Close</AsnButton>
-          <AsnButton className="primary">Add Filter</AsnButton>
-
-        </Space>
+      <Form form={form} onFinish={onFinish} >
+        <Collapse
+          collapsible="icon"
+          bordered={false}
+          accordion
+          expandIcon={({ isActive }) => (isActive ?? false ? '-' : '+')}
+        >
+          <Panel header="Age" key="1" className="fffffffffff">
+            <Form.Item name='age' rules={[{ required: true }]}>
+              <Slider range marks={marks} defaultValue={[16, 27]} />
+            </Form.Item>
+          </Panel>
+          <Panel header="Gender" key="2">
+            <Form.Item name='gender'>
+              <Radio.Group onChange={onChange} value={value}>
+                <Space direction="vertical">
+                  <Radio value={'FEMALE'}>Female</Radio>
+                  <Radio value={'MALE'}>Male</Radio>
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          </Panel>
+          <Panel header="Student" key="3">
+            <Form.Item name='student'>
+              <Radio.Group onChange={onChangeStudent} value={valueStudent}>
+                <Space direction="vertical">
+                  <Radio value={true}>Nonstudent</Radio>
+                  <Radio value={false}>Student</Radio>
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          </Panel>
+          <Panel header="Status" key="4">
+            <Form.Item name='status'>
+              <AsnCheckboxGroup
+                style={{ width: '200px' }}
+                options={optionsStatus}
+                onChange={onChangeStatus}
+              />
+            </Form.Item>
+          </Panel>
+          <Panel header="Paid job" key="5">
+            <Form.Item name='paid_job'>
+              <Radio.Group onChange={onChangePaid} value={valuePaid}>
+                <Space direction="vertical">
+                  <Radio value={true}>Paid job</Radio>
+                  <Radio value={false}>Unemployed</Radio>
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          </Panel>
+          <Panel header="Vulnerability" key="6">
+            <Form.Item name='vulnerability'>
+              <Radio.Group
+                onChange={onChangeVulnerability}
+                value={valueVulnerability}
+              >
+                <Space direction="vertical">
+                  <Radio value={true}>NA</Radio>
+                  <Radio value={false}>Vulnerable group member</Radio>
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+          </Panel>
+          <Panel header="Region" key="7">
+            <Form.Item name='region'>
+              <AsnCheckboxGroup
+                style={{ width: '160px' }}
+                options={optionsRegion}
+                onChange={onChangeRegion}
+              />
+            </Form.Item>
+          </Panel>
+        </Collapse>{' '}
+        <Form.Item>
+          <Space
+            direction="horizontal"
+            size={[10, 0]}
+            style={{
+              justifyContent: 'space-around',
+              width: '100%',
+              padding: '30px 0 0px 0px'
+            }}
+          >
+            <AsnButton className="default">Close</AsnButton>
+            <AsnButton className="primary" htmlType="submit">Add Filter</AsnButton>
+          </Space>{' '}
+        </Form.Item>
+      </Form>
     </ContentAssingersFilter>
   );
   const columns: ColumnsType<DataType> = [
     {
-      title: <Popover
-      content={content}
-      title="Filter your results"
-      trigger="click"
-      overlayClassName='applicantsFilter'
-    >
-      <Button type='link' style={{ padding: '0' }}><FilterOutlined className='filterIcon'/></Button>
-    </Popover>,
+      title: (
+        <Popover
+          content={content}
+          title="Filter your results"
+          trigger="click"
+          overlayClassName="applicantsFilter"
+        >
+          <Button type="link" style={{ padding: '0' }}>
+            <FilterOutlined className="filterIcon" />
+          </Button>
+        </Popover>
+      ),
       key: 'filter',
       width: 50
     },
@@ -243,62 +327,69 @@ const ApplicantsDataList: React.FC<ApplicatnList> = ({ allApplicants, searchApli
   const today = new Date();
 
   {
-    (Boolean((allApplicants?.result))) && search.length < 2
+    Boolean(allApplicants?.result) && search.length < 2
       ? (
-    <div>
-    { allApplicants?.result?.map((k: any) => (
-      data.push({
-        key: `${k?.id}`,
-        name: `${k?.fullName}`,
-        age: `${Number(moment?.(today)?.format('YYYY').valueOf()) - Number(moment(k?.dob).format('YYYY'))}`,
-        education: `${k?.educationLevel}`,
-        sector: `${k?.courseMap?.course?.sector?.title}`,
-        course: `${k?.courseMap?.course?.title}`,
-        status: `${k?.courseMap?.status}`,
-        region: `${k?.region}`,
-        phoneNumber: `${k?.phone}`,
-        gender: `${k?.gender}`,
-        student: `${k?.student}`,
-        vulnerability: `${k?.vulnerabilities}`,
-        workOrganisation: `${k?.workOrganisation}`,
-        informedAboutUs: `${k?.informedAboutUs}`
-      })
-    )
-
-    )
-}
-</div>
+      <div>
+        {allApplicants?.result?.map((k: any) =>
+          data.push({
+            key: `${k?.id}`,
+            name: `${k?.fullName}`,
+            age: `${
+              Number(moment?.(today)?.format('YYYY').valueOf()) -
+              Number(moment(k?.dob).format('YYYY'))
+            }`,
+            education: `${k?.educationLevel}`,
+            sector: `${k?.courseMap?.course?.sector?.title}`,
+            course: `${k?.courseMap?.course?.title}`,
+            status: `${k?.courseMap?.status}`,
+            region: `${k?.region}`,
+            phoneNumber: `${k?.phone}`,
+            gender: `${k?.gender}`,
+            student: `${k?.student}`,
+            vulnerability: `${k?.vulnerabilities}`,
+            workOrganisation: `${k?.workOrganisation}`,
+            informedAboutUs: `${k?.informedAboutUs}`
+          })
+        )}
+      </div>
         )
       : (
-    <div>    {searchAplicant?.result?.map((s: any) => (
-      data.push({
-        key: `${s?.id}`,
-        name: `${s?.fullName}`,
-        age: `${Number(moment?.(today)?.format('YYYY').valueOf()) - Number(moment(s?.dob).format('YYYY'))}`,
-        education: `${s?.educationLevel}`,
-        sector: `${s?.courseMap?.course?.sector?.title}`,
-        course: `${s?.courseMap?.course?.title}`,
-        status: `${s?.courseMap?.status}`,
-        region: `${s?.region}`,
-        phoneNumber: `${s?.phone}`,
-        gender: `${s?.gender}`,
-        student: `${s?.student}`,
-        vulnerability: `${s?.vulnerabilities}`,
-        workOrganisation: `${s?.workOrganisation}`,
-        informedAboutUs: `${s?.informedAboutUs}`
-      })
-    )
-
-    )
-}
-</div>
+      <div>
+        {' '}
+        {searchAplicant?.result?.map((s: any) =>
+          data.push({
+            key: `${s?.id}`,
+            name: `${s?.fullName}`,
+            age: `${
+              Number(moment?.(today)?.format('YYYY').valueOf()) -
+              Number(moment(s?.dob).format('YYYY'))
+            }`,
+            education: `${s?.educationLevel}`,
+            sector: `${s?.courseMap?.course?.sector?.title}`,
+            course: `${s?.courseMap?.course?.title}`,
+            status: `${s?.courseMap?.status}`,
+            region: `${s?.region}`,
+            phoneNumber: `${s?.phone}`,
+            gender: `${s?.gender}`,
+            student: `${s?.student}`,
+            vulnerability: `${s?.vulnerabilities}`,
+            workOrganisation: `${s?.workOrganisation}`,
+            informedAboutUs: `${s?.informedAboutUs}`
+          })
+        )}
+      </div>
         );
   }
   return (
     <>
-    <Table columns={columns} dataSource={ data } rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' : 'table-row-dark'}/>
-
-</>
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowClassName={(record, index) =>
+          index % 2 === 0 ? 'table-row-light' : 'table-row-dark'
+        }
+      />
+    </>
   );
 };
 export default ApplicantsDataList;
