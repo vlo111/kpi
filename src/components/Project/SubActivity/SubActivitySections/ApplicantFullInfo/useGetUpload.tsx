095d74c6e4980a07b2268cd34aabe119fde00192
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2023-02-14 16:40:15 +0400

    export function applicants list

--- src/components/Project/SubActivity/SubActivitySections/ApplicantFullInfo/useGetUpload.tsx
+++ src/components/Project/SubActivity/SubActivitySections/ApplicantFullInfo/useGetUpload.tsx
@@ -1,16 +1,40 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
 
-import client from '../../../../../api/client';
-
-const url = 'api/sub-activity/course/:id/download/applicants';
 const useAttacheFiles: any = (options = {}) =>
-  useMutation(
-    async (id: any) => {
-      if (id !== undefined) {
-        const data = await client.post(url.replace(':id', id));
-        return data;
-      }
-    },
-    options
-  );
+  useMutation(async (id: any) => {
+    if (id !== undefined) {
+      try {
+        var filename = "";
+        const token: string | null = JSON.parse(
+          localStorage.getItem("token") as string
+        );
+
+        fetch(
+          `https://apimeetk.analysed.ai/api/sub-activity/course/${id}/download/applicants`,
+          {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+          .then((result) => {
+            if (!result.ok) {
+              throw Error(result.statusText);
+            }
+            return result.blob();
+          })
+          .then((blob) => {
+            if (blob != null) {
+              var url = window.URL.createObjectURL(blob);
+              var a = document.createElement("a");
+              a.href = url;
+              a.download = filename;
+              document.body.appendChild(a);
+              a.click();
+              a.remove();
+            }
+          });
+        return {};
+      } catch (error) {}
+    }
+  }, options);
 export default useAttacheFiles;
