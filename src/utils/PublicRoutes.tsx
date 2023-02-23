560a1ab7158ae4d422ac35c418213dfd0a048e9e
vlo111 <vvcsharp@gmail.com>
2023-02-23 17:21:13 +0400

    remove dashboard from menu

--- src/utils/PublicRoutes.tsx
+++ src/utils/PublicRoutes.tsx
@@ -1,12 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import React from 'react';
 import { useAuth } from '../hooks/useAuth';
+import { useProject } from '../hooks/useProject';
 
 export const PublicRoutes: React.FC = () => {
   const { user } = useAuth();
 
+  const { projectId }: { projectId: string } = useProject();
+
   if (user != null) {
-    return <Navigate to="/dashboard" />;
+    return <Navigate to={`/project/overview/${projectId}`} />;
   }
 
   return (
