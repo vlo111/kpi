b6100327d3147af6b02ed2af2257d8f2d667f09a
@arkad.amiryan <arkad.amiryan@gmail.com>
2023-03-13 12:42:42 +0400

    Remuve useEffect from QuestionContent component

--- src/components/AssessmentForm/QuestionContent/index.tsx
+++ src/components/AssessmentForm/QuestionContent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Typography } from 'antd';
 import { AsnForm } from '../../Forms/Form';
