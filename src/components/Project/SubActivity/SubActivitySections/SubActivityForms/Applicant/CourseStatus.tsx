c7872ac4d86684b83300d772150ebb90f46188f2
Hovhannes Davtyan <hovhannes.davtyan@analysed.ai>
2023-03-20 11:38:52 +0400

    error message

--- src/components/Project/SubActivity/SubActivitySections/SubActivityForms/Applicant/CourseStatus.tsx
+++ src/components/Project/SubActivity/SubActivitySections/SubActivityForms/Applicant/CourseStatus.tsx
@@ -46,7 +46,6 @@ const CourseStatusForm: React.FC<any> = ({
       setActiveKey('1');
     },
     onError: () => {
-      console.log('aaa');
     }
   });
 
@@ -57,9 +56,11 @@ const CourseStatusForm: React.FC<any> = ({
         StartCourse({ id: courseId });
       }
     },
-    onError: () => {
-      void message.error('Access denied');
-    }
+    onError: ({
+      response: {
+        data: { message: error }
+      }
+    }: { response: { data: { message: string } } }) => message.error(error, 2)
   });
 
   const add = (): any => {
