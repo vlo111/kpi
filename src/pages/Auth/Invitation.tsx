587a6b8662618ba3f516f5767a1dcdbab004b965
@arkad.amiryan <arkad.amiryan@gmail.com>
2023-04-26 17:46:23 +0400

    Fixed Invitation error

--- src/pages/Auth/Invitation.tsx
+++ src/pages/Auth/Invitation.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate, useNavigate, useSearchParams } from 'react-router-dom';
-import { Row, Col, Space, message } from 'antd';
+import styled from 'styled-components';
+
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Row, Col, Space, message, Typography } from 'antd';
 
 import { AsnInput } from '../../components/Forms/Input';
 import { AsnForm } from '../../components/Forms/Form';
@@ -12,21 +14,52 @@ import {
   VALIDATE_MESSAGES,
   PATHS
 } from '../../helpers/constants';
-import { IResetPassword, ISuccessMessage } from '../../types/auth';
+import {
+  IResetPassword,
+  ISuccessMessage,
+  ITokenMessage
+} from '../../types/auth';
 import useGetIntivitationResult from '../../api/UserProfile/useGetIntivitationResult';
 import useGetIntivitationToken from '../../api/UserProfile/useGetIntivitationToke';
 import { TitleAuth } from '../../components/Layout/TitleAuth';
 
+import { ReactComponent as NotAccessSvg } from '../../assets/icons/error_404.svg';
+
+const { Title } = Typography;
+
+const NotAccessContent = styled(Space)`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .ant-space-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h5,
+  h3 {
+    color: var(--dark-1);
+    font-size: 20px;
+    font-weight: var(--font-normal);
+  }
+  h5 {
+    font-size: 14px !important;
+  }
+`;
+
 const Invitation: React.FC = () => {
   const [error, setError] = useState<string>('');
+  const [errorToken, setErrorToken] = useState<string>('');
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const token = searchParams.get('token');
 
   const { mutate: invitationPasswordToken }: any = useGetIntivitationToken({
     onSuccess: () => {},
-    onError: () => {
-      <Navigate to={`/${PATHS.ERROR_500}`} />;
+    onError: (error: ITokenMessage) => {
+      setErrorToken(error.response.data.message);
     }
   });
   useEffect(() => {
@@ -50,6 +83,7 @@ const Invitation: React.FC = () => {
     const { password, confirmPassword } = values;
     invitationPassword({ password, repeatPassword: confirmPassword, token });
   };
+
   const rulesConfirmPassword = [
     {
       required: true
@@ -65,49 +99,63 @@ const Invitation: React.FC = () => {
       }
     })
   ];
+
   return (
     <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
-      <Col span={8} style={{ maxWidth: '460px' }}>
-        <AsnForm
-          form={form}
-          onFinish={onFinish}
-          validateMessages={VALIDATE_MESSAGES}
-          layout="vertical"
-        >
-          <TitleAuth>Accept Invitation</TitleAuth>
-          {error?.length > 0 && <AsnAlert type="error" message={error} />}
-          <AsnForm.Item
-            name="password"
-            label="New password"
-            rules={[
-              { required: true },
-              { min: 8, max: 16 },
-              { pattern: passwordRegExp }
-            ]}
+      {(token === null || errorToken === 'Invalid token') && (
+        <NotAccessContent direction="vertical">
+          <NotAccessSvg />
+          <Title level={3}>We are sorry,</Title>
+          <Title level={5}>but the invitation is expired.</Title>
+        </NotAccessContent>
+      )}
+      {token !== null && errorToken !== 'Invalid token' && (
+        <Col span={8} style={{ maxWidth: '460px' }}>
+          <AsnForm
+            form={form}
+            onFinish={onFinish}
+            validateMessages={VALIDATE_MESSAGES}
+            layout="vertical"
           >
-            <AsnInput.Password placeholder="New password" />
-          </AsnForm.Item>
-          <AsnForm.Item
-            name="confirmPassword"
-            label="Confirm New Password"
-            rules={rulesConfirmPassword}
-          >
-            <AsnInput.Password placeholder="Confirm New Password" />
-          </AsnForm.Item>
-          <AsnForm.Item>
-            <Space size="middle" style={{ width: '100%' }} direction="vertical">
-              <AsnButton
-                htmlType="submit"
-                className="primary"
+            <TitleAuth>Accept Invitation</TitleAuth>
+            {error?.length > 0 && <AsnAlert type="error" message={error} />}
+            <AsnForm.Item
+              name="password"
+              label="New password"
+              rules={[
+                { required: true },
+                { min: 8, max: 16 },
+                { pattern: passwordRegExp }
+              ]}
+            >
+              <AsnInput.Password placeholder="New password" />
+            </AsnForm.Item>
+            <AsnForm.Item
+              name="confirmPassword"
+              label="Confirm New Password"
+              rules={rulesConfirmPassword}
+            >
+              <AsnInput.Password placeholder="Confirm New Password" />
+            </AsnForm.Item>
+            <AsnForm.Item>
+              <Space
+                size="middle"
                 style={{ width: '100%' }}
-                loading={isLoading}
+                direction="vertical"
               >
-                Submit
-              </AsnButton>
-            </Space>
-          </AsnForm.Item>
-        </AsnForm>
-      </Col>
+                <AsnButton
+                  htmlType="submit"
+                  className="primary"
+                  style={{ width: '100%' }}
+                  loading={isLoading}
+                >
+                  Submit
+                </AsnButton>
+              </Space>
+            </AsnForm.Item>
+          </AsnForm>
+        </Col>
+      )}
     </Row>
   );
 };
