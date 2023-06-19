18d1ef164cd648fdd060b2a32954aa5db5281141
@arkad.amiryan <arkad.amiryan@gmail.com>
2023-06-19 15:40:13 +0400

    Cleaning code

--- src/components/Applicant/Tabs/Tab/Content/Courses/Course/Files/index.tsx
+++ src/components/Applicant/Tabs/Tab/Content/Courses/Course/Files/index.tsx
@@ -15,14 +15,9 @@ import {
   PATHS
 } from '../../../../../../../../helpers/constants';
 import { ReactComponent as ResendSvg } from '../../Icons/resend.svg';
-import { ReactComponent as DeleteSvg } from '../../../../../../../../assets/icons/delete.svg';
-import { EyeOutlined } from '@ant-design/icons';
 import { IFiles, IHistory } from '../../../../../../../../types/applicant';
-import { Row, Space, Tooltip, message } from 'antd';
-import { useQueryClient } from '@tanstack/react-query';
-import useDeleteFile from '../../../../../../../../api/Files/useDeleteFile';
-import { IErrorMessage } from '../../../../../../../../types/project';
 import FilesPreviewModal from '../../../../../../../Project/SubActivity/SubActivitySections/Files/FilesPreviewModal';
+import FileMap from './FileMap';
 
 const Modal = styled(AsnModal)`
   .ant-modal-content {
@@ -34,17 +29,6 @@ const Modal = styled(AsnModal)`
   }
 `;
 
-const SingleFile = styled(Row)`
-  &:hover {
-    background: var(--primary-light-2);
-  }
-  padding: 2px;
-  svg {
-    height: 21px;
-    width: 16px;
-  }
-`;
-
 const Files: React.FC<IFiles> = ({ applicantId, history }) => {
   const [openPreviewApplicant, setOpenPreviewApplicant] = useState('');
   const [openPreview, setOpenPreview] = useState<boolean>(false);
@@ -52,7 +36,6 @@ const Files: React.FC<IFiles> = ({ applicantId, history }) => {
   const [openPreviewAssessmentForm, setOpenPreviewAssessmentForm] =
     useState<boolean>(false);
   const navigate = useNavigate();
-  const queryClient = useQueryClient();
 
   const { mutate: resendApplicant } = useResendApplicant('');
 
@@ -68,35 +51,6 @@ const Files: React.FC<IFiles> = ({ applicantId, history }) => {
         )
     }
   );
-
-  const { mutate: DeleteFile } = useDeleteFile({
-    onSuccess: () => {
-      void queryClient.invalidateQueries([
-        'api/applicant/:id/course/:sectionDataId/history'
-      ]);
-    }
-  });
-
-  const deleteDoc = (name: string): void => {
-    DeleteFile(name, {
-      onSuccess: () => {
-        void queryClient.invalidateQueries(['/api/sub-activity']);
-        void message.success('Successfully deleted', 2);
-      },
-      onError: ({
-        response: {
-          data: { message: errorMessage }
-        }
-      }: IErrorMessage) => {
-        void message.error(errorMessage, 2);
-      }
-    });
-  };
-
-  const handlePreview = (path: string): void => {
-    setOpenPreview(true);
-    setUrl(path);
-  };
 
   const hasActiveForm = forms[0];
 
@@ -205,48 +159,13 @@ const Files: React.FC<IFiles> = ({ applicantId, history }) => {
             </div>
           </div>
       )}
-      {history.id !== undefined &&
-        history.files.map((f) => {
-          let fileTitle: string = f?.originalName?.slice(0, 18);
-          if (f?.originalName?.length > 18) {
-            fileTitle += '...';
-          }
-
-          return (
-            <SingleFile
-              key={f.originalName}
-              justify={'space-between'}
-              wrap={false}
-            >
-              <Space.Compact
-                style={{ gap: '8px', fontSize: 'var(--base-font-size)' }}
-              >
-                <Tooltip placement="top" title={f?.originalName}>
-                  <a
-                    key={f.originalName}
-                    className="file"
-                    href={f.path}
-                    target="_blank"
-                    download={f.path}
-                    rel="noreferrer"
-                  >
-                    {fileTitle}
-                  </a>
-                </Tooltip>
-              </Space.Compact>
-              <Space.Compact style={{ gap: '8px' }}>
-                <DeleteSvg
-                  style={{ width: '9px', cursor: 'pointer' }}
-                  onClick={() => deleteDoc(f.name)}
-                />
-                <EyeOutlined
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => handlePreview(f?.path)}
-                />
-              </Space.Compact>
-            </SingleFile>
-          );
-        })}
+      {history.id !== undefined && (
+        <FileMap
+          history={history}
+          setOpenPreview={setOpenPreview}
+          setUrl={setUrl}
+        />
+      )}
       <Modal
         footer={false}
         open={openPreviewApplicant !== ''}
