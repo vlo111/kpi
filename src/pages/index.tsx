cbc14470249608c501f23dd9c50ebb4158b57908
vlo111 <vvcsharp@gmail.com>
2023-06-29 19:36:17 +0400

    fix data

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Index: React.FC = () => {
-  return (
-        <>
-            index
-        </>
-  )
-}
--- src/pages/sign/SignOut.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SignOut: React.FC = () => {
-  return (
-        <div>
-            sign out
-        </div>
-  )
-}
